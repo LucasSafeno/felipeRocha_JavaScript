cae238574fa1d6a1fae32714523c0a39a8412c8f
Lucas Safeno <lugnorio@gmail.com>
2025-09-13 08:24:04 -0300

    Curso de JavaScript #02 - Variáveis & Tipos de Dados

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+// var message = 'Hello World'; // Não muito usado.Pois não tem Escopo de bloco
 
+// const -> Não podemos alterar
+// const message = 'Hello World';
+// // alert(message);
+
+// // let -> Consegue alterar
+// let message2 = 'Hello World';
+// alert(message2);
+// message2 = 'String modificada';
+// alert(message2);
+
+// const message = 'Hello Wold';
+// console.log(message.length);
+
+// const firstName = 'Lucas';
+// const lastName = 'Tenório';
+
+// const names = 'Lucas,Thauanna,Daniel';
+
+// console.log('Meu nome é ' + firstName + ' ' + lastName);
+// console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
+// console.log(names.split(','));
+
+// const number = 5;
+// console.log(number.toString());
+// console.log(typeof number);
+
+// Lista
+
+const list = [1, 2, 3, 4, 5];
+const object = { name: 'Lucas' };
